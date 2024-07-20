54bdf9c0b87c368cd0f7762acc7923481f379477
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-07-20 23:14:25 +0300

    changes

--- index.js
+++ index.js
@@ -9,15 +9,53 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
 }
 
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(event => event.date === date);
+    let timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function allWagesFor() {
+    let eligibleDates = this.timeInEvents.map(event => event.date);
+    let payable = eligibleDates.reduce((memo, date) => memo + wagesEarnedOnDate.call(this, date), 0);
+    return payable;
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((memo, record) => memo + allWagesFor.call(record), 0);
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => employee.firstName === firstNameString);
+}
